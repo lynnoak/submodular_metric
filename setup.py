bf151a485c3b564a7ccd9cdbffec3f7ea33ece6f
Hoel LE CAPITAINE <hoel.lecapitaine@univ-nantes.fr>
2016-12-15 14:14:35 +0100

    Upload new file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+from Cython.Distutils import build_ext
+import numpy
+
+extensions = [Extension("*", ["src/*.pyx"])]
+
+setup(
+	cmdclass = {'build_ext':build_ext},
+	ext_modules = cythonize(extensions),
+	include_dirs =[numpy.get_include()]
+	)
